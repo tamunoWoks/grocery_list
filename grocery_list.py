b0711a4978d76eb60565c637d43dbda67e3d0725
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-10 00:59:40 +0100

    Check if user input is or is not in grocery dict and add with count

--- grocery_list.py
+++ grocery_list.py
@@ -1,4 +1,12 @@
 def main():
+    #create empty dictionary
+    groceries = {}
     while True:
         #Collect user input, remove whitespace and Make uppercase
         item = input().strip().upper()
+        #Check if user input is or is not in grocery dict and add with count
+        if item not in groceries:
+            groceries[item] = 1
+        else:
+            groceries[item] += 1
+
