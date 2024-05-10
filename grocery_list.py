61e45826a5c5ef74b3bce1b370a0af69ecfac95a
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-10 01:05:18 +0100

    Catch EOFError when user exits program and print sorted grocery list with item count

--- grocery_list.py
+++ grocery_list.py
@@ -2,11 +2,21 @@ def main():
     #create empty dictionary
     groceries = {}
     while True:
-        #Collect user input, remove whitespace and Make uppercase
-        item = input().strip().upper()
-        #Check if user input is or is not in grocery dict and add with count
-        if item not in groceries:
-            groceries[item] = 1
-        else:
-            groceries[item] += 1
+        try:
+            #Collect user input, remove whitespace and Make uppercase
+            item = input().strip().upper()
+            #Check if user input is or is not in grocery dict and add with count
+            if item not in groceries:
+                groceries[item] = 1
+            else:
+                groceries[item] += 1
+        #Catch EOFError when user exits program and print sorted list with count
+        except EOFError:
+            for key in sorted(groceries):
+                print(groceries[key], key)
+            #Exit while loop
+            break
 
+
+if __name__ == "__main__":
+    main()
